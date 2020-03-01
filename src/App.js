237f154d927417acabb6ffe9dcbbd68db62beed4
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-03-01 19:10:06 +0200

    add 18 lesson

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.scss'
 import { Header } from './Components/Header/Header';
 import { Navbar } from './Components/Navbar/Navbar';
 import { Profile } from './Components/Profile/Profile';
+import {Dialogs} from "./Components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
+      <div className="app-wrapper__content">
       <Profile />
+      <Dialogs greeting="hello dialogs!"/>
+      </div>
     </div>
     );
 }
---
+++ src/Components/Dialogs/Dialogs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Dialogs = (props) => {
+    return (
+        <div>{props.greeting}</div>
+    )
+}
