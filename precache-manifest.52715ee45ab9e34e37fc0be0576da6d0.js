e18a8c5334d817985eb5e78247882e6d57bb5e52
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-05-22 16:06:32 +0300

    Updates

--- precache-manifest.52715ee45ab9e34e37fc0be0576da6d0.js
+++ precache-manifest.52715ee45ab9e34e37fc0be0576da6d0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b53319898ef53b3a30227de1763d7d44",
+    "revision": "e57b79f6e49442fde27b0eb9d2b6872d",
     "url": "/react_social-network/index.html"
   },
   {
-    "revision": "e8c226e179286fd758d6",
+    "revision": "adc3b85401b178396d34",
     "url": "/react_social-network/static/css/main.60623d0b.chunk.css"
   },
   {
-    "revision": "1d224bb0d45d725591fd",
-    "url": "/react_social-network/static/js/2.d1c4784c.chunk.js"
+    "revision": "f2c9340514db402dc415",
+    "url": "/react_social-network/static/js/2.fd806cbb.chunk.js"
   },
   {
     "revision": "bf23039b4e70a07c5aeb1b4010df3572",
-    "url": "/react_social-network/static/js/2.d1c4784c.chunk.js.LICENSE.txt"
+    "url": "/react_social-network/static/js/2.fd806cbb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e8c226e179286fd758d6",
-    "url": "/react_social-network/static/js/main.bf39dbc4.chunk.js"
+    "revision": "adc3b85401b178396d34",
+    "url": "/react_social-network/static/js/main.460ecc5e.chunk.js"
   },
   {
     "revision": "d32345e13d9311ab0b15",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_social-network/precache-manifest.449dc77887e058fee41fb63ace8ca777.js"
+  "/react_social-network/precache-manifest.52715ee45ab9e34e37fc0be0576da6d0.js"
 );
 
 self.addEventListener('message', (event) => {
