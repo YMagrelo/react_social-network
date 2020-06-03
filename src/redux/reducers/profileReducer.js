c674eda341b09f13006de2fda215b9c433b7161f
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-06-03 23:35:14 +0300

    add reverse method to user posts

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -8,6 +8,9 @@ const SET_PROFILE_STATUS = 'SET_PROFILE_STATUS';
 const initialState = {
   posts: [
     { id: 1, post: 'Hello, how are you?', likes: 4 },
+    { id: 2, post: 'Hello, i\'m fine', likes: 5 },
+    { id: 3, post: 'empty line', likes: 1 },
+    { id: 4, post: 'My girl, my girl don\'t lie to me, tell me where did you sleep last night, in the pines, in the pines where the sun don\'t ever shine, i would shiver the whole night through', likes: 1 },
   ],
   profile: null,
   status: '',
